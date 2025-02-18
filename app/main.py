2ad7ed654e86c9ac4d8c283cb3b311cdc679ebe6
MacIntel <macintel@MacIntels-MacBook-Pro.local>
2025-02-18 16:06:39 -0700

    setup POST /receipts/processes endpoint accepting receipt payload

--- app/main.py
+++ app/main.py
@@ -1,8 +1,25 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+class Item(BaseModel):
+    shortDescription: str
+    price: str
+
+class Receipt(BaseModel):
+    retailer: str
+    purchaseDate: str
+    purchaseTime: str
+    total: str
+    items: list[Item]
+
+
+# @app.get("/")
+# def root():
+#     return {"message": "Hello World"}
+
+@app.post("/receipts/process")
+def root(receipt: Receipt):
+    return {"message": "receipts/process"}
