ac657537b63adb6296a0e0cc9e8c6fdac9f9f924
MacIntel <macintel@MacIntels-MacBook-Pro.local>
2025-02-18 17:52:18 -0700

    update /receipts/process endpoint to save receipts to a list

--- app/main.py
+++ app/main.py
@@ -1,14 +1,18 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import uuid
+from typing import Optional
 
 app = FastAPI()
+
+receipts = []
 
 class Item(BaseModel):
     shortDescription: str
     price: str
 
 class Receipt(BaseModel):
+    id: Optional[str] = None
     retailer: str
     purchaseDate: str
     purchaseTime: str
@@ -17,9 +21,12 @@ class Receipt(BaseModel):
 
 @app.post("/receipts/process")
 def create_receipt_id(receipt: Receipt):
-    print(receipt)
+    #print(receipt)
     id_generator = uuid.uuid4()
-    return {"id": id_generator}
+    receipt.id = id_generator
+    receipts.append(receipt)
+    print(receipts)
+    return {"id": receipts[-1].id}
 
 @app.get("/receipts/{id}/points")
 def get_receipt_points(id: str):
